be3345426993ed26c8d1a6a13bb7b05d9a0bb3ab
Anton Strandqvist <anton.sjolander@hyperisland.se>
2023-04-03 15:38:51 +0200

    aded db connection

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize(
+	`postgres://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.URL}:${process.env.PORT}/${process.env.DBNAME}`
+);
 const app = express();
+
+const main = async () => {
+	try {
+		await sequelize.authenticate();
+		console.log("Connection has been established successfully.");
+	} catch (error) {
+		console.error("Unable to connect to the database:", error);
+	}
+};
 
 app.use(morgan("tiny"));
 
@@ -25,3 +39,5 @@ app.post("/post", (req, res) => {
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
+
+main();
