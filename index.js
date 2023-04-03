5cd6271b6682e19eab578d3aca4ba65d7f53b809
Anton Strandqvist <anton.sjolander@hyperisland.se>
2023-04-03 16:36:01 +0200

    added patch and post

--- index.js
+++ index.js
@@ -2,16 +2,33 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = new Sequelize(
 	`postgres://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.URL}:${process.env.PORT}/${process.env.DBNAME}`
 );
+
 const app = express();
+
+const Task = sequelize.define("Task", {
+	id: {
+		type: DataTypes.UUID,
+		defaultValue: DataTypes.UUIDV4,
+		primaryKey: true,
+	},
+	title: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	completed: {
+		type: DataTypes.BOOLEAN,
+	},
+});
 
 const main = async () => {
 	try {
 		await sequelize.authenticate();
 		console.log("Connection has been established successfully.");
+		// await sequelize.sync({ force: true });
 	} catch (error) {
 		console.error("Unable to connect to the database:", error);
 	}
@@ -21,19 +38,26 @@ app.use(morgan("tiny"));
 
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
+app.get("/tasks", async (req, res) => {
+	const tasks = await Task.findAll();
+	res.json(tasks.map((t) => t.toJSON()));
+
+	console.log(tasks);
 });
 
-app.get("/about", (req, res) => {
-	res.send(`<p style="color:red">HELLO</p>`);
+app.post("/task", async (req, res) => {
+	const { title, completed } = req.body;
+	const task = await Task.create({ title, completed });
+	res.json(task.toJSON());
 });
 
-app.post("/post", (req, res) => {
-	const { message } = req.body;
-	res.send(`here is your message: ${message}`);
+app.patch("/task/:id", async (req, res) => {
+	const { completed } = req.body;
+	const { id } = req.params;
+	await Task.update({ completed }, { where: { id } });
+	res.end();
 });
 
 app.listen(port, () => {
